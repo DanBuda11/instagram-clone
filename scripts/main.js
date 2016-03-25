d2bfb0841f6ca3ab4d5790588daa13a10379f543
Dan Buda <danbuda@gmail.com>
2016-03-25 14:13:20 -0500

    We've got Kittens

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,17 @@
+// Goals: save images added to tiny pizza server; get images/captions from server and post using append, etc
+// validate upon submit that the url is actually a url and that the caption field is not empty
+// make input form menu slide up/down on click
+// cancel button closes form as well
+// ADD IMAGE button posts to the array on the server then closes the input form
+// Set up an error function that prints something to the website screen and not just a console.log
 
 // This is my function that runs upon GET success and creates all the contents of the photos & captions
 var dataGet = function(data) {
 	var container = $('#get-results');
-	data.results.forEach(function(val, i, arr) {
+	data.forEach(function(val, i, arr) {
 		var postBox = $('<div class="postBox"></div>');
 		var imgBox = $('<div class="imgBox"></div>');
-		var img = $('<img/>').html(val.image);
+		var img = $('<img>', {src: val.image});
 		var captionBox = $('<div class="captionBox"></div>');
 		var caption = $('<p></p>').html(val.caption);
 
@@ -13,6 +19,7 @@ var dataGet = function(data) {
 		captionBox.append(caption);
 		postBox.append(imgBox);
 		postBox.append(captionBox);
+		container.append(postBox);
 	});
 }
 
@@ -22,19 +29,18 @@ var getResults = {
 	type: 'get',
 	dataType: 'json',
 	success: dataGet,
-	error: getError
+	error: function(err) {
+		console.log('Error! Error!')
+	}
 };
-
 // This is the POST ajax request which will add a new photo/caption 
-var postResults = {
-	url: 'http://small-tiyfe.herokuapp.com/collections/danstagram',
-	type: 'post',
-	dataType: 'json',
-	success: dataPost,
-	error: postError
-};
-
-
+// var postResults = {
+// 	url: 'http://small-tiyfe.herokuapp.com/collections/danstagram',
+// 	type: 'post',
+// 	dataType: 'json',
+// 	success: dataPost,
+// 	error: console.log('Error! Error!')
+// };
 
 // This is the actual running of the GET request
 // This should happen after each post is complete to refresh the page, as well as when the page is initially loaded
@@ -43,4 +49,4 @@ $.ajax(getResults);
 
 // This is the actual running of the POST request
 // This should happen once submit button is clicked and the validation for URL & caption is passed as true
-$.ajax.(postResults);
+// $.ajax.(postResults);
